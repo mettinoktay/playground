c527c6e53e482a2036b561565e5de4a8014b5609
mettinoktay <mettinoktay@gmail.com>
2022-07-10 21:46:33 +0300

    cleanup

--- stm8/sdcc-stm8/pwm.c
+++ stm8/sdcc-stm8/pwm.c
@@ -1,36 +1,5 @@
 #include "stm8s.h"
 
-/*
-
-Pulse width modulation mode allows you to generate a signal with a frequency determined
-by the value of the TIM1_ARR register and a duty cycle determined by the value of the
-TIM1_CCRi registers.
-
-The PWM mode can be selected independently on each channel (one PWM per OCi output)
-by writing 110 (PWM mode 1) or 111 (PWM mode 2) in the OCiM bits in the TIM1_CCMRi
-registers. The corresponding preload register must be enabled by setting the OCiPE bits in
-the TIM1_CCMRi registers. The auto-reload preload register (in up-counting or center-
-aligned modes) may be optionally enabled by setting the ARPE bit in the TIM1_CR1
-register.
-
-As the preload registers are transferred to the shadow registers only when an UEV occurs,
-all registers have to be initialized by setting the UG bit in the TIM1_EGR register before
-starting the counter.
-
-OCi polarity is software programmable using the CCiP bits in the TIM1_CCERi registers. It
-can be programmed as active high or active low. The OCi output is enabled by a
-combination of CCiE, MOE, OISi, OSSR and OSSI bits (TIM1_CCERi and TIM1_BKR
-registers). Refer to the TIM1_CCERi register descriptions for more details.
-
-In PWM mode (1 or 2), TIM1_CNT and TIM1_CCRi are always compared to determine
-whether TIM1_CCRi ≤ TIM1_CNT or TIM1_CNT≤ TIM1_CCRi (depending on the direction
-of the counter).
-
-The timer is able to generate PWM in edge-aligned mode or center-aligned mode
-depending on the CMS bits in the TIM1_CR1 register.
-
-*/
-
 #define FREQ_16MHZ (char) 0
 typedef unsigned char uint8_t;
 
